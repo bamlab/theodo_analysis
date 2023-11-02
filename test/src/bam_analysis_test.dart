2513417bb3da78d8f1f06104eff45136f66f56c1
MaximeRougieux <maximer@bam.tech>
2023-11-02 09:41:04 +0100

    refactor: :rotating_light: Fix lint issue

--- test/src/bam_analysis_test.dart
+++ test/src/bam_analysis_test.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
-import 'package:flutter_test/flutter_test.dart';
 import 'package:bam_analysis/bam_analysis.dart';
+import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('BamAnalysis', () {
