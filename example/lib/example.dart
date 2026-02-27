b45774a0d70c5f1707da232495d5e9db35a46ddf
MaximeRougieux <maximer@bam.tech>
2026-02-27 11:56:11 +0100

    fix: :rotating_light: example lint

--- example/lib/example.dart
+++ example/lib/example.dart
@@ -4,7 +4,7 @@
 void main() {
   /// The following line would normally show a lint warning
   /// but we can disable the lint rule for this line using the following syntax.
-  var greeting = 'hello world'; // ignore: prefer_final_locals
+  var greeting = 'hello world'; // ignore: prefer_final_locals // <- testing
 
   /// The following line would normally show a lint warning
   /// but we can disable the lint rule for this file using `ignore_for_file`.
